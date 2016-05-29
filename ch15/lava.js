a8f5faa0dd5c0c3dac2e7f0c139564265141c267
Rasmus Kuschel <rasmushofmann@gmail.com>
2016-05-29 17:15:25 +0200

    ch15 vector operations

--- ch15/lava.js
+++ ch15/lava.js
@@ -28,6 +28,16 @@ function Level(plan) {
 	this.status = this.finishDelay = null;
 }
 
+function Vector(x,y) {
+	this.x = x; this.y = y;
+}
+Vector.prototype.plus = function(other) {
+	return new Vector(this.x + other.x, this.y + other.y);
+}
+Vector.prototype.times = function(factor) {
+	return new Vector(this.x * factor, this.y * factor);
+}
+
 var simpleLevelPlan = [
 	"                         ",
 	"                         ",
