8af13b0bda884875fab1e4f66528aec19da3412e
Rasmus Kuschel <rasmushofmann@gmail.com>
2016-05-30 09:50:24 +0200

    level animate

--- ch15/lava.js
+++ ch15/lava.js
@@ -28,6 +28,53 @@ function Level(plan) {
 	this.status = this.finishDelay = null;
 };
 
+Level.prototype.obstacleAt = function(pos, size) {
+	var xStart = Math.floor(pos.x);
+	var xEnd = Math.ceil(pos.x + size.x);
+	var yStart = Math.floor(pos.y);
+	var yEnd = Math.ceil(pos.y + size.y);
+
+	if (xStart < 0 || xEnd > this.width || yStart < 0) {
+		return "wall";
+	}
+	if (yEnd > this.height) {
+		return "lava";
+	}
+	for (var y=yStart; y<yEnd; y++) {
+		for (var x=xStart; x<xEnd; x++) {
+			var fieldType = field[y][x];
+			if (fieldType) return fieldType;
+		}
+	}
+}
+
+Level.prototyp.actorAt = function(actor) {
+	for (var i=0; i<this.actors.length; i++) {
+		var other = this.actors[i];
+		if (other != actor &&
+			actor.pos.x + actor.size.x > other.pos.x &&
+			actor.pos.x < other.pos.x - other.size.x &&
+			actor.pos.y + actor.size.y > other.pos.y &&
+			actor.pos.x < other.pos.y - other.size.y)
+			return other;
+	}
+}
+
+var maxStep = 0.05;
+
+Level.prototype.animate = function(step, keys) {
+	if (this.status != null) {
+		this.finishDelay -= step;
+	}
+	while (step > 0) {
+		var thisStep = Math.min(step, maxStep);
+		this.actors.forEach(function(actor) {
+			actor.act(thisStep, this, keys);
+		}, this);
+		step -= thisStep;
+	}
+}
+
 var actorChars = {
 	"@" : Player,
 	"o" : Coin,
