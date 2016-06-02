411dcea0f14d1165ba0b0020e3e70235e0159d3a
Rasmus Kuschel <rasmushofmann@gmail.com>
2016-06-02 13:54:59 +0200

    ch14 tabs event listener

--- ch14/tabs.js
+++ ch14/tabs.js
@@ -2,28 +2,46 @@
 	var elems = document.getElementsByClassName("tabs");
 	for (var i=0; i<elems.length; i++) {
 		var elem = elems[i];
-		var elemName = elem.getAttribute("name");
+		var groupId = elem.getAttribute("id");
 		var select = document.createElement("div");
-		elem.appendChild(select);
+		elem.insertBefore(select, elem.children[0]);
 
-		console.log(elem);
-		for (var j=0; j<elem.children.length-1; j++) {
+		for (var j=1; j<elem.children.length; j++) {
 			var child = elem.children[j];
-			var name = child.getAttribute("name");
+			child.style.display = "none";
+			var childId = child.getAttribute("id");
 
 			var choice = document.createElement("input");
 			choice.type = "radio";
-			choice.name = elemName;
-			choice.value = name;
+			choice.name = groupId;
+			choice.value = childId;
 
 			var label = document.createElement("label");
 			label.appendChild(choice);
-			label.innerHTML += name;
+			label.innerHTML += childId;
+			label.addEventListener("click", function(event) {
+				if (event.target) {
+					var gId = event.target.name;
+					var cId = event.target.value;
+
+					if (gId && cId) {
+						console.log(gId + ":" + cId);
+
+						var g = document.getElementById(gId);
+						if (g) {
+							for (var c=1; c<g.children.length; c++) {
+								g.children[c].style.display = "none";
+							}
+						}
+						var c = document.getElementById(cId);
+						if (c) {
+							c.style.display = "block";
+						}
+					}
+				}
+			});
 
 			select.appendChild(label);
 		}
-
-		// nur aktives child darstellen
-		// listener zum umstellen
 	}
 })();
