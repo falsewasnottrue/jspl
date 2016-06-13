7adb46becedbe2734a2e2b54d36e19cc3186871f
Rasmus Kuschel <rasmushofmann@gmail.com>
2016-06-13 12:03:55 +0200

    ch22 first working version

--- ch22/find_path.js
+++ ch22/find_path.js
@@ -8,9 +8,8 @@ Vector.prototype.times = function(factor) {
   return new Vector(this.x * factor, this.y * factor);
 }
 
-function GraphNode(pos, edges) {
-  this.pos = new Vector(Math.random() * 1000,
-                        Math.random() * 1000);
+function GraphNode(id, edges) {
+  this.id = id;
   this.edges = [];
 }
 GraphNode.prototype.connect = function(other) {
@@ -24,9 +23,10 @@ GraphNode.prototype.hasEdge = function(other) {
 };
 
 function treeGraph(depth, branches) {
+  var id = 0;
   var graph = [];
   function buildNode(depth) {
-    var node = new GraphNode();
+    var node = new GraphNode(id++);
     graph.push(node);
     if (depth > 1)
       for (var i = 0; i < branches; i++)
@@ -38,8 +38,45 @@ function treeGraph(depth, branches) {
 }
 
 function findPath(graph, start, end) {
+  function findPathRecursive(paths, i) {
+    console.log("paths: " + paths);
+    console.log("i: " + i);
+
+    if (i + 1 > paths.length) {
+      return []; // no path, maybe undefined?
+    }
+    var path = paths[i];
+    var endNode = path[path.length - 1];
+    if (path[0] === start && endNode === end) {
+      return path;
+    }
+
+    // 1. find all nodes connected to endNode
+    var nodes = endNode.edges
+    console.log(nodes);
+
+    // 2. filter out those that are already in path
+    var newNodes = nodes.filter(function(node) {
+      return !(path.includes(node));
+    });
+    console.log(nodes);
+
+    // 3. for each node add path+node to paths
+    newNodes.forEach(function(newNode) {
+      console.log("createNewPaths with " + newNode);
+      console.log("path: " + path);
+      console.log("path.slice(); " + path.slice());
+      var newPath = path.slice();
+      newPath.push(newNode);
+      console.log("newPath: " + newPath);
+      paths.push(newPath);
+    });
+
+    return findPathRecursive(paths, i+1);
+  }
+
   var paths = [[start]];
-  return false;
+  return findPathRecursive(paths, 0);
 }
 
 var graph = treeGraph(3,2);
